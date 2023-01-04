1d71be3f1ab593fad687ba37ce9b0b0af05f4dd8
user.email <aromito7@gmail.com>
2023-01-04 12:16:57 -0500

    Fixed account value error

--- react-app/src/components/LandingPage/AccountGraph.js
+++ react-app/src/components/LandingPage/AccountGraph.js
@@ -1,9 +1,10 @@
 
 const AccountGraph = ({user}) => {
-    console.log(user)
+    const accountValue = user.portfolio.reduce((acc, stock) => acc + stock.shares * stock.stock.price ,0)
+    console.log(accountValue)
     return(
         <div id="account-graph-container">
-            <h1>${(user.account_value + user.buying_power).toFixed(2)}</h1>
+            <h1>${(accountValue + user.buying_power).toFixed(2)}</h1>
             <div className="flex">
                 <p className="green-font">$0.00 (0.00%)</p>
                 <p>Today</p>
