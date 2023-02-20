24492a4f6efd544d8b9250fbb1f6d9315e8535a7
user.email <aromito7@gmail.com>
2023-02-20 03:37:15 -0500

    fixed stockpage bugs

--- react-app/src/components/StockPage/StockNews.js
+++ react-app/src/components/StockPage/StockNews.js
@@ -14,14 +14,14 @@ const StockNews = ({stock}) => {
 
                             const height = thumbnail.height
                             const width = thumbnail.width
-                            console.log(height, width)
                         }
+
                         return(
                             <div className="news-article light-background-hover cursor-pointer" key={i} onClick={e => window.open(article.link, '_blank')}>
                                 <div>
                                     <div className="flex">
                                         <div>{article.publisher}</div>
-                                        <div className="grey-font left-margin10">{new Date(article.providerPublishTime).toString().split(' ').slice(1,3)}</div>
+                                        <div className="grey-font left-margin10">{new Date(article.providerPublishTime * 1000).toString().split(' ').slice(1,3).join(' ')}</div>
                                     </div>
                                     <div className="bold">{article.title}</div>
                                 </div>
