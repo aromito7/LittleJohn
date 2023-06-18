c161e5a58140cb0b9eb5971921ad0e7f1b57f703
user.email <aromito7@gmail.com>
2023-06-18 12:32:17 -0400

    updated the stock route for accessing recent stock data

--- app/api/stock_routes.py
+++ app/api/stock_routes.py
@@ -125,6 +125,35 @@ def use_yfinance_api(symbol):
 
             # 'yfinance-version': yf.__version__}
 
+def update_stock(old_stock, stock_info):
+    old_stock.price = stock_info['price']
+    old_stock.history = stock_info['history']
+    old_stock.open = stock_info['open_price']
+    old_stock.is_up = stock_info['is_up']
+    old_stock.delta = stock_info['delta']
+    
+    old_stock.delta = stock_info['eps']
+    old_stock.about = stock_info['about']
+    old_stock.employees = stock_info['employees']
+    old_stock.city = stock_info['city']
+    old_stock.state = stock_info['state']
+    old_stock.sector = stock_info['sector']
+    old_stock.industry = stock_info['industry']
+    old_stock.website = stock_info['website']
+    old_stock.share = stock_info['shares']
+    old_stock.year_high = stock_info['year_high']
+    old_stock.year_low = stock_info['year_low']
+    old_stock.day_high = stock_info['day_high']
+    old_stock.day_low = stock_info['day_low']
+    old_stock.market_cap = stock_info['market_cap']
+    old_stock.volume = stock_info['volume']
+    old_stock.average_volume = stock_info['average_volume']
+    old_stock.news = stock_info['news']
+
+
+    db.session.commit()
+
+
 @stock_routes.route('/<symbol>', methods=['GET'])
 def get_stock_info(symbol): #add name to parameters for seeding data
     """
@@ -137,7 +166,34 @@ def get_stock_info(symbol): #add name to parameters for seeding data
 
     stock = Stock.query.filter(Stock.symbol == symbol).first()
     if stock:
-        print(stock.to_dict())
+        presentDate = datetime.now()
+        updated_at = stock.updated_at
+        updated_timestamp = datetime.timestamp(updated_at) * 1000
+        unix_timestamp = datetime.timestamp(presentDate)*1000
+
+        elapsed_milli = unix_timestamp - updated_timestamp
+
+        if elapsed_milli > 1000 * 60 * 5:
+            try:
+                stock_data = use_yfinance_api(symbol)
+            except:
+                error = {'error' : 'stock not found'}
+                print(error)
+                return stock.to_dict()
+
+            update_stock(stock, stock_data)
+
+            return stock.to_dict()
+
+        # return {
+        #     'now': unix_timestamp,
+        #     'updated_at': updated_timestamp,
+        #     'elapsed': unix_timestamp - updated_timestamp,
+        #     '5 minute interval': 1000 * 5 * 60
+        # }
+
+
+        # print(stock.to_dict())
         return stock.to_dict()
 
     print("HELLO YFINANCE API!")
